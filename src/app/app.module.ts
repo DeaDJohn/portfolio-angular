9f76700993a501cd58fb769a1b17face3dd9b859
CPU casa <juanjofb86@gmail.com>
2018-07-18 22:17:29 +0200

    Creado los componentes necesarios, cargado los stilos en el angular.json.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { routing, appRoutingProvides } from "./app.routing";
 
 import { AppComponent } from './app.component';
+import { AboutComponent } from './components/about/about.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { CreateComponent } from './components/create/create.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { ErrorComponent } from './components/error/error.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ProjectsComponent,
+    CreateComponent,
+    ContactComponent,
+    ErrorComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, routing],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent, appRoutingProvides]
 })
-export class AppModule { }
+export class AppModule {}
