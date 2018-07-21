a5f52ff037c3d324e9271564ddf3bcef0adbc34f
Juanjo Fernandez <juanjofb86@gmail.com>
2018-07-21 09:03:08 +0200

    Guardar imagen subida desde el formulario y obtener el listados de proyectos.

--- src/app/components/create/create.component.ts
+++ src/app/components/create/create.component.ts
@@ -1,46 +1,60 @@
 import { Component, OnInit } from '@angular/core';
 import { Projects } from '../../models/projects';
 import { ProjectService } from "../../services/project.service";
+import { UploadService } from '../../services/upload.service';
+import { Global } from "../../services/global";
 
 
 @Component({
-  selector: 'app-create',
-  templateUrl: './create.component.html',
-  styleUrls: ['./create.component.css'],
-  providers: [ProjectService]
+	selector: "app-create",
+	templateUrl: "./create.component.html",
+	styleUrls: ["./create.component.css"],
+	providers: [ProjectService, UploadService]
 })
 export class CreateComponent implements OnInit {
-  public title: string;
-  public project: Projects;
-  public status: string;
-  constructor(
-    private _projectService: ProjectService
-  ) {
-    this.title = 'Crear Proyecto';
-    this.project = new Projects('','','','',2019,'','');
-   }
+	public title: string;
+	public project: Projects;
+	public status: string;
+	public filesToUpload: Array<File>;
 
+	constructor(
+		private _projectService: ProjectService,
+		private _uploadService: UploadService
+	) {
+		this.title = "Crear Proyecto";
+		this.project = new Projects("", "", "", "", 2019, "", "");
+	}
 
-  ngOnInit() {
+	ngOnInit() {}
+	onSubmit(form) {
+		console.log(this.project);
 
-  }
-  onSubmit(form){
-    console.log(this.project);
-    this._projectService.saveProject(this.project).subscribe(
-      response =>{
-        if(response.project){
-          this.status = "success";
-          form.reset();
-        }else{
-          this.status = "failed"
-        }
-        console.log(response.project);
-      },
-      error=>{
-        console.log(<any>error);
-      }
-      
-    );
-  }
+		// Guardar los datos
+		this._projectService.saveProject(this.project).subscribe(
+			response => {
+				if (response.project) {
+					
 
+					// Subir la imagen
+					this._uploadService.makeFileRequest(Global.url + "upload-image/" + response.project._id, [], this.filesToUpload, 'image')
+						.then((result:any)=>{
+							console.log(result);
+							this.status = "success";
+							form.reset();
+						});
+
+				} else {
+					this.status = "failed";
+				}
+			},
+			error => {
+				console.log(<any>error);
+			}
+		);
+	}
+
+	fileChangeEvent(fileInput: any  ){
+		console.log(fileInput);
+		this.filesToUpload = <Array<File>>fileInput.target.files;
+	}
 }
--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { Projects } from "../../models/projects";
+import { ProjectService } from '../../services/project.service';
+import { Global } from "../../services/global";
 
 @Component({
-  selector: 'app-projects',
-  templateUrl: './projects.component.html',
-  styleUrls: ['./projects.component.css']
+	selector: "app-projects",
+	templateUrl: "./projects.component.html",
+	styleUrls: ["./projects.component.css"],
+	providers: [ProjectService]
 })
 export class ProjectsComponent implements OnInit {
+	public projects: Projects[];
+	public url: string;
+	constructor(
+		private _projectService: ProjectService
+	) {
+		this.url = Global.url;
+	}
 
-  constructor() { }
+	ngOnInit() {
+		this.getProjects();
+	}
 
-  ngOnInit() {
-  }
-
+	getProjects(){
+	    this._projectService.getProjects().subscribe(
+	      response =>{
+	        this.projects = response.projects;
+	        console.log(response);
+	      },
+	      error =>{
+	        console.log(<any>error);
+	      }
+	    );
+	}
 }
