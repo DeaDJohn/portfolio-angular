8f720fc76de9f00605537da85ec53832aa332cb1
Juanjo Fernandez <juanjofb86@gmail.com>
2018-07-22 21:22:49 +0200

    Prueba de añadir una animacion a los elementos a la entrada.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { FormsModule } from "@angular/forms";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from "./material";
 
+
 import { AppComponent } from './app.component';
 import { AboutComponent } from './components/about/about.component';
 import { ProjectsComponent } from './components/projects/projects.component';
--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -2,20 +2,43 @@ import { Component, OnInit } from '@angular/core';
 import { Projects } from "../../models/projects";
 import { ProjectService } from '../../services/project.service';
 import { Global } from "../../services/global";
+import {
+	trigger,
+	state,
+	style,
+	animate,
+	transition
+} from '@angular/animations';
 
 @Component({
 	selector: "app-projects",
 	templateUrl: "./projects.component.html",
 	styleUrls: ["./projects.component.css"],
-	providers: [ProjectService]
+	providers: [ProjectService],
+	animations: [
+		trigger('flyInOut', [
+			state('in', style({ 
+				transform: 'translateX(0)' })),
+			transition('void => *', [
+				style({ transform: 'translateX(-100%)' }),
+				animate(100)
+			]),
+			transition('* => void', [
+				animate(100, style({ transform: 'translateX(100%)' }))
+			])
+		])
+	]
 })
 export class ProjectsComponent implements OnInit {
 	public projects: Projects[];
 	public url: string;
+	public show_bool: boolean;
+	public show: string;
 	constructor(
 		private _projectService: ProjectService
 	) {
 		this.url = Global.url;
+		this.show_bool = false;
 	}
 
 	ngOnInit() {
@@ -32,5 +55,13 @@ export class ProjectsComponent implements OnInit {
 	        console.log(<any>error);
 	      }
 	    );
+	}
+
+	showProject(){
+		return this.show_bool? 'in' : 'void';
+	}
+
+	toggleState(){
+		this.show_bool = !this.show_bool;
 	}
 }
